ebb50c5f3d204c13f55e51f0c2e1844319971f8f
vasunaidudola <108747125+vasunaidudola@users.noreply.github.com>
2025-11-26 19:29:48 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+  default     = "ap-south-1"
+}
+
+variable "dockerhub_username" {
+  type        = string
+  description = "Docker Hub username"
+}
+
+variable "image_name" {
+  type        = string
+  description = "Docker image name"
+  default     = "java-ec2-demo"
+}
+
+variable "image_tag" {
+  type        = string
+  description = "Docker image tag"
+  default     = "latest"
+}
