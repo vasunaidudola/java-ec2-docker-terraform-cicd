c9d5404e212c500f1eb947ff8420856d96293045
vasunaidudola <108747125+vasunaidudola@users.noreply.github.com>
2025-11-26 21:28:58 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0"
 
   backend "s3" {
-    bucket         = "vasu-terraform-state-11262025"  # <-- your bucket
+    bucket         = "vasu-terraform-state-123456"  # <-- change to your real bucket name
     key            = "ec2-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
@@ -26,7 +26,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Subnets in default VPC (new data source)
+# Subnets in default VPC
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -73,18 +73,27 @@ resource "aws_instance" "java_app" {
   subnet_id              = data.aws_subnets.default.ids[0]
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
+  # Use templatefile and include deploy_version
   user_data = templatefile("${path.module}/user_data.sh", {
     dockerhub_username = var.dockerhub_username
     image_name         = var.image_name
     image_tag          = var.image_tag
+    deploy_version     = var.deploy_version
   })
+
+  # If user_data changes, replace instance (forces redeploy)
+  user_data_replace_on_change = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
     Name = "java-docker-ec2"
   }
 }
 
-# Output
+# Output public IP
 output "instance_public_ip" {
   value = aws_instance.java_app.public_ip
 }
