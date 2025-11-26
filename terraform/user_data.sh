2f40c7995c40e366b7adc809c62900f0cfd30141
vasunaidudola <108747125+vasunaidudola@users.noreply.github.com>
2025-11-26 21:30:33 +0530

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# Deploy version: ${deploy_version}
+
 yum update -y
 amazon-linux-extras install docker -y || yum install docker -y
 systemctl enable docker
 systemctl start docker
 
+# Pull latest image
 docker pull ${dockerhub_username}/${image_name}:${image_tag}
 
+# Stop and remove old container if it exists
+docker rm -f java-app || true
+
+# Run container
 docker run -d --name java-app -p 80:8080 --restart always ${dockerhub_username}/${image_name}:${image_tag}
